fd6049ecbfcd133053586374d338c11ef31b059d
JFocus <x137186008@163.com>
2018-03-23 09:11:25 +0800

    copy xiang's code to make some changes

--- include/myslam/map.h
+++ include/myslam/map.h
@@ -17,35 +17,27 @@
  *
  */
 
-#ifndef CAMERA_H
-#define CAMERA_H
+#ifndef MAP_H
+#define MAP_H
 
 #include "myslam/common_include.h"
+#include "myslam/frame.h"
+#include "myslam/mappoint.h"
 
 namespace myslam
 {
-
-// Pinhole RGBD camera model
-class Camera
+class Map
 {
 public:
-    typedef std::shared_ptr<Camera> Ptr;
-    float   fx_, fy_, cx_, cy_, depth_scale_;  // Camera intrinsics 
+    typedef shared_ptr<Map> Ptr;
+    unordered_map<unsigned long, MapPoint::Ptr >  map_points_;        // all landmarks
+    unordered_map<unsigned long, Frame::Ptr >     keyframes_;         // all key-frames
 
-    Camera();
-    Camera ( float fx, float fy, float cx, float cy, float depth_scale=0 ) :
-        fx_ ( fx ), fy_ ( fy ), cx_ ( cx ), cy_ ( cy ), depth_scale_ ( depth_scale )
-    {}
-
-    // coordinate transform: world, camera, pixel
-    Vector3d world2camera( const Vector3d& p_w, const SE3& T_c_w );
-    Vector3d camera2world( const Vector3d& p_c, const SE3& T_c_w );
-    Vector2d camera2pixel( const Vector3d& p_c );
-    Vector3d pixel2camera( const Vector2d& p_p, double depth=1 ); 
-    Vector3d pixel2world ( const Vector2d& p_p, const SE3& T_c_w, double depth=1 );
-    Vector2d world2pixel ( const Vector3d& p_w, const SE3& T_c_w );
-
+    Map() {}
+    
+    void insertKeyFrame( Frame::Ptr frame );
+    void insertMapPoint( MapPoint::Ptr map_point );
 };
-
 }
-#endif // CAMERA_H
+
+#endif // MAP_H
